26ab9b227a2af0aa370a5ffbaba30146eaefdcbb
Amy Haddad <amy.m.haddad@gmail.com>
2019-01-25 08:10:23 -0500

    updated version of intro_computation file

--- intro_computation.py
+++ intro_computation.py
@@ -1,10 +1,18 @@
 
-user_numbers = int(input("Enter 10 numbers: "))
 
-list_odd_numbers = []
-for number in user_numbers:
-    divisibility_checker = number % 2
-    if divisibility_checker != 0:
-        list_odd_numbers.append(divisibility_checker)
-print(max(list_odd_numbers))
 
+# Need to use the split here bc in next function we need to look at each item, not the string as a whole.
+def number_generator():
+    return input("Enter 10 numbers separated by a space: ").split()
+
+user_numbers = number_generator()
+
+#First, need to go through each number and change the type from string to int
+def largest_odd_number(user_numbers):
+    user_numbers_as_ints =[int(number) for number in user_numbers] 
+    return max([number for number in user_numbers_as_ints if number % 2 != 0])
+ 
+
+x = largest_odd_number(user_numbers)
+print(x)
+    
