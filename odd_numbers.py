8855b3ec6031f72118fdf90d315d4b9f63fcd27f
Amy Haddad <amy.m.haddad@gmail.com>
2019-01-25 08:19:21 -0500

    final updates to odd_numbers

--- odd_numbers.py
+++ odd_numbers.py
@@ -1,6 +1,4 @@
 
-
-
 # Need to use the split here bc in next function we need to look at each item, not the string as a whole.
 def number_generator():
     return input("Enter 10 numbers separated by a space: ").split()
@@ -8,11 +6,11 @@ def number_generator():
 user_numbers = number_generator()
 
 #First, need to go through each number and change the type from string to int
+#Then, I find the odd numbers
 def largest_odd_number(user_numbers):
-    user_numbers_as_ints =[int(number) for number in user_numbers] 
+    user_numbers_as_ints =[int(number) for number in user_numbers]
     return max([number for number in user_numbers_as_ints if number % 2 != 0])
  
-
 x = largest_odd_number(user_numbers)
 print(x)
     
