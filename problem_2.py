c836f747f967d659cc8c122e210ae8364e48c048
Amy Haddad <amy.m.haddad@gmail.com>
2019-02-02 07:49:47 -0500

    3 ways to solve problem_2

--- problem_2.py
+++ problem_2.py
@@ -11,26 +11,28 @@
 # print(count)
 
 #Solved problem using for loop
-x = 'azcbobobegghakl'
+# s = 'azcbobobegghakl'
 
-x_len = len(x)
+# s_len = len(s)
+# count = 0
+
+# for i in range(s_len):
+#     if s[i:i+3] == 'bob':
+#         count += 1
+# print("Number of times bob occurs is: {}".format(count))
+
+
+s = 'bobcbobbmjnbboobobobobkbobobobm'
+
+s_len = len(s)
 count = 0
-
-for i in range(x_len):
-    if x[i:i+3] == 'bob':
-        count += 1
-print(count)
-
-
-
-   
+i = 0
     
-
-        
-
-
-
-
-
-    
-
+while i < s_len:
+    for i in range(s_len):
+        if s[i:i+3] == 'bob':
+            count += 1
+            i += 1
+    if i == s_len - 1:
+        break
+print("Number of times bob occurs is: {}".format(count))
