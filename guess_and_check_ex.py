6d49a7516b4e3d7d1031b0c3f09c39543c4cf4de
Amy M Haddad <amy.m.haddad@gmail.com>
2019-02-06 17:45:50 -0500

    added notes to guess and check exercise

--- guess_and_check_ex.py
+++ guess_and_check_ex.py
@@ -2,7 +2,7 @@
 # user_number = int(input("Please think of a number between 0 and 100!"))
 
 print("Please think of a number between 0 and 100!")
-secret_number = 42
+# user_input = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
 low = 0
 high = 100
 guess = (low + high) // 2
@@ -11,7 +11,12 @@ while low <= high:
     print("Is your secret number "+ str(guess)+ "?")
     guess_evaluation = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ")
 
-    if guess == secret_number:
+#Always check for validity first b/c otherwise it can get trapped. 
+
+#Don't need these first conditionals b/c it's the user who decides whether or not the number is high or low or correct
+#What i'm testing wtih the conditionals below is what to do based on the user input. 
+##
+    # if guess == secret_number:
         if guess_evaluation == 'c':
             print("Game over. Your secret number was:", guess)
             break
