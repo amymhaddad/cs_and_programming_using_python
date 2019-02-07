7f7515e15e8f3400332fe5f86d816a73eef171b8
Amy M Haddad <amy.m.haddad@gmail.com>
2019-02-07 13:19:32 -0500

    adding function practice file

---
+++ function_practice.py
@@ -0,0 +1,20 @@
+# def is_even(i):
+#     print("hi")
+#     return i % 2 == 0
+
+# is_even(3)
+
+# x = 1.2
+
+# def a(x):
+#     return x + 1
+
+# print(a(x))
+
+def f(x):
+    x = x +1
+    print(f"In f(x): x = {x}")
+    return x
+
+x = 3
+z = f(x)
