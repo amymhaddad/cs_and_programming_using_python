6ae6f54b832e660bb1d7e44c0925bdfdb6dba7ac
Amy M Haddad <amy.m.haddad@gmail.com>
2019-02-08 17:25:25 -0500

    many function examples

--- function_practice.py
+++ function_practice.py
@@ -1,20 +1,103 @@
+
+
 # def is_even(i):
 #     print("hi")
 #     return i % 2 == 0
 
 # is_even(3)
 
-# x = 1.2
+# def f(y):
+#     y = y + 1
+#     print("In f(x), x = ", y)
+#     return y
 
-# def a(x):
-#     return x + 1
+# x = 3
+# z = f(x)
 
-# print(a(x))
 
-def f(x):
-    x = x +1
-    print(f"In f(x): x = {x}")
-    return x
+# def func_a():
+#     print('inside func_a')
+# print(func_a())
 
-x = 3
-z = f(x)
+# def func_b(y):
+#     print("inside func_b")
+#     return y
+# print(5 + func_b(2))
+
+# def func_c(z):
+#     print('inside func_c')
+#     return z()
+# print(func_c(func_a))
+
+# def f(y):
+#     x = 1
+#     x += 1
+#     print(x)
+
+# x = 5
+# f(x)
+# print(x)
+
+# def g(y):
+#     print(y)
+#     print(y + 1)
+
+# x = 5
+# g(x)
+# print(x)
+
+# def h(y):
+#     x = x + 1
+#     # print('in h(x) x = ', x)
+#     # return x
+    
+
+# x = 5
+# h(x)
+# print(x)
+# print(x)
+
+# def g(x):
+#     def h():
+#         x = 'abc'
+#     x = x + 1
+#     print("in g(x): x = ", x)
+
+#     return x
+
+# x = 3
+# z = g(x)
+
+# def h(x):
+#     x = x + 1
+
+# x = 5
+# h(x)
+# print(x)
+
+
+def h(y):
+    x = x + 1
+
+x = 5
+h(x)
+print(x)
+
+
+
+
+# x = 5
+# f(x)
+# print(x)
+#Here, None doesn't print out b/c I'm not printing the function itself
+
+# def g(x):
+#     def h():
+#         x = 'abc'
+#     x = x + 1
+#     print('in g(x): x - ', x)
+#     h()
+#     return x
+
+# x = 3
+# z = g(x)
