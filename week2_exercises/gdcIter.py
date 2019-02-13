51bc74d6b9975cb468979d8ca1fb9f1fc1261cd5
Amy M Haddad <amy.m.haddad@gmail.com>
2019-02-13 11:08:55 -0500

    adding new file -- gdcIter.py

---
+++ week2_exercises/gdcIter.py
@@ -0,0 +1,30 @@
+
+a = 5
+b = 12
+test_val = 0
+
+
+if a < b:
+    test_val = a
+elif a > b:
+    test_val = b
+
+#Need to find the larger of the two numbers, a or b
+#THen compare: see if larger_number % test_val == 0
+
+
+
+# if b != a:
+#     if b > a:
+#         b = larger
+#         a = smaller
+
+#     else:
+#         a = larger
+#         b = smaller
+    
+# if larger % test_val == 0:
+#     print(larger)
+# else:
+#     test_val -= 1
+
