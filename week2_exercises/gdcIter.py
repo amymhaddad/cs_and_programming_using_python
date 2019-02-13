6ccfbad0a9fbfde18ce407e4bd533315302a9e2d
Amy M Haddad <amy.m.haddad@gmail.com>
2019-02-13 11:35:30 -0500

    completed gdcIter.py exercise

--- week2_exercises/gdcIter.py
+++ week2_exercises/gdcIter.py
@@ -1,30 +1,18 @@
 
-a = 5
+a = 8
 b = 12
 test_val = 0
-
 
 if a < b:
     test_val = a
 elif a > b:
     test_val = b
 
-#Need to find the larger of the two numbers, a or b
-#THen compare: see if larger_number % test_val == 0
+highest_val = max(a, b)
 
-
-
-# if b != a:
-#     if b > a:
-#         b = larger
-#         a = smaller
-
-#     else:
-#         a = larger
-#         b = smaller
-    
-# if larger % test_val == 0:
-#     print(larger)
-# else:
-#     test_val -= 1
-
+for number in range(test_val+1):
+    if highest_val % test_val == 0:
+        print(test_val)
+        break
+    else:
+        test_val -= 1
