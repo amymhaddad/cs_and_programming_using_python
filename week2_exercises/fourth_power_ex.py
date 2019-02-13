0e709c1a794f50b7cdd3a6782debf1f3ad0850b4
Amy M Haddad <amy.m.haddad@gmail.com>
2019-02-13 08:53:26 -0500

    update to fourth power

--- week2_exercises/fourth_power_ex.py
+++ week2_exercises/fourth_power_ex.py
@@ -1,3 +1,5 @@
+#Write a Python function, fourthPower, that takes in one number and returns that value raised to the fourth power.
+#You should use the square procedure that you defined in an earlier exercise (you don't need to redefine square in this box; when you call square, the grader will use our definition).
 
 def square(number):
     '''
@@ -7,19 +9,11 @@ def square(number):
 
 # square(3)
 
-
-
 def fourthPower(x):
     '''
     x: int or float.
     '''
-    square(x)
+    # square(x)
     return square(x) * square(x)
 
 print(fourthPower(2))
-
-    
-  
-
-
-
