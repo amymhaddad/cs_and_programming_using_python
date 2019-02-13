a688b329aa0446b7a86545f30720f3090f7effad
Amy M Haddad <amy.m.haddad@gmail.com>
2019-02-13 09:49:33 -0500

    completed power_iter and power_recur exercises

--- week2_exercises/power_iter_ex.py
+++ week2_exercises/power_iter_ex.py
@@ -1,13 +1,16 @@
+#Write an iterative function iterPower(base, exp) that calculates the exponential by simply using successive multiplication.
 
-# def iterPower(base, exp):
-#     result = 1
+#Solution 1 
+def iterPower(base, exp):
+    result = 1
 
-#     for i in (range(exp)):
-#         result *= base
-#     return result
+    for i in (range(exp)):
+        result *= base
+    return result
 
-# print(iterPower(4, 4))
+print(iterPower(4, 4))
 
+#Solution 2
 def iterPower(base, exp):
     result = 1
 
--- week2_exercises/power_recur_ex.py
+++ week2_exercises/power_recur_ex.py
@@ -1,8 +1,10 @@
+
+#Write a function recurPower(base, exp) which computes the base recursively
+
 def recurPower(base, exp):
-    result = 1
     if exp == 0:
         return 1
     else:
-        return result*=base recurPower(exp - 1, result)
-
-print(recurPower(2, 3))
+        return base * recurPower(base, exp - 1)
+    
+print(recurPower(4, 4))
