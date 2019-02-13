d61300327ee421dd54ef8e159f0a705067249246
Amy M Haddad <amy.m.haddad@gmail.com>
2019-02-13 08:32:57 -0500

    moved files to new folder

--- methods_as_functions.py
+++
@@ -1,8 +0,0 @@
-# str1 = 'exterminate!' 
-# print(str1.upper())
-
-
-str1 = "this is string example....wow!!!";
-str2 = "exam";
-
-print(str1.index(str2))
--- practice.py
+++ practice.py
@@ -1,10 +1,38 @@
 
-end = 6
-current = 1
-total = 0
+# end = 6
+# new_total = 0
+# counter = 0
 
-while current <= end:
-    total += current
-    current += 1
+# while counter < end:
+#     new_total += end
+#     counter += 1
 
-print(total)
+# print(new_total)
+
+
+# def iterPower(base, exp):
+#     result = 1
+
+#     while exp > 0:
+#         result *= base
+#         exp -= 1
+#     return result
+
+# print(iterPower(2, 0))
+
+def iterPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+ 
+    returns: int or float, base^exp
+    '''
+    result = 1
+    i = 0
+
+    while i < exp:
+        result *= base
+        i += 1
+    return result
+
+print(iterPower(2.2, 2))
