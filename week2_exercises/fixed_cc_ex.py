d1d812cff6c8139e57298dd08d0692ea2cc9c7a3
Amy M Haddad <amy.m.haddad@gmail.com>
2019-02-25 08:26:22 -0500

    updated fixed_cc ex. Need to test if there's a remaining balance at end of 12 month

--- week2_exercises/fixed_cc_ex.py
+++ week2_exercises/fixed_cc_ex.py
@@ -1,26 +1,47 @@
 
+import math
+
+# def debt_payoff(balance, annualInterestRate):
+
+balance = 3329
+annualInterestRate = 0.2
+
+
 
 def debt_payoff(balance, annualInterestRate):
-    months = 12
+    month = 1
+    monthly_payment = balance / 12
+    if monthly_payment % 2 != 0:
+        monthly_payment_multiple_of_ten = (monthly_payment // 10) * 10 + 10
+    else:
+        monthly_payment_multiple_of_ten = monthly_payment
     
-    for mon in range(months):
-        monthly_payment = balance // 12
-        monthly_payment_multiple_of_ten = (monthly_payment // 10) * 10
-        monthly_unpaid_balance = balance - monthly_payment_multiple_of_ten
-
+    for month in range(13):
+        monthly_unpaid_balance = balance - monthly_payment_multiple_of_ten    
         monthly_interest_rate = (annualInterestRate) / 12.0
-        new_balance = monthly_unpaid_balance + (monthly_interest_rate * monthly_unpaid_balance)
         
-        balance = new_balance 
+        new_balance = monthly_unpaid_balance + (monthly_interest_rate * monthly_unpaid_balance)   
+        balance = new_balance
+        month += 1
+
+        if balance <= 0:
+            break
+        else:
+            remaining_balance = (monthly_payment_multiple_of_ten + balance) / 12
+            print(remaining_balance)
+        #     monthly_payment_multiple_of_ten = monthly_payment_multiple_of_ten + remaining_balance
 
 
-    # rounded_balance = round(balance, 2)
-    # final_output = "Remaining balance: " + str(rounded_balance)
-    return balance 
+#HERE for updating the remaining balance and adding this balance to the monthly payment, make this monthly payment a multiple of 10 and see when balance is 0 (break)
+ 
+    return monthly_payment_multiple_of_ten
 
-print(debt_payoff(3329, 0.2))
+print(debt_payoff(balance, annualInterestRate))
 
 
-# Monthly unpaid balance = (Previous balance) - (Minimum fixed monthly payment)
+# print(debt_payoff(3329, 0.2))
+#lowest payment = 310
+
+
 # Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
 
