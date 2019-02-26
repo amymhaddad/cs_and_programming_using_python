981b0a01455bcd8955b03a5fb6c8aa3c68db7c05
Amy M Haddad <amy.m.haddad@gmail.com>
2019-02-26 09:34:14 -0500

    final changes to v2 fixed cc ex

--- week2_exercises/v2_fixed_cc_ex.py
+++ week2_exercises/v2_fixed_cc_ex.py
@@ -1,31 +1,27 @@
 
 
 
-def debt_payoff(balance, annualInterestRate, montly_payment):
+def calculate_end_balance(balance, annualInterestRate, monthly_payment):
     month = 1
-    
     while month <= 12:
         monthly_interest_rate = (annualInterestRate) / 12.0
-        unpaid_balance = balance - fixed_montly_payment
-        new_balance = (unpaid_balance) + (monthly_interest_rate * unpaid_balance)
+        monthly_unpaid_balance = balance - monthly_payment
+        new_balance = (monthly_unpaid_balance) + (monthly_interest_rate * monthly_unpaid_balance)
+
         balance = new_balance
         month += 1
 
-    ending_balance = round(balance, 2)
-    final_output = "Remaining balance: " + str(ending_balance)
-    return ending_balance
+    return balance
 
-def find_monthly_payment(balance, annualInterestRate):
-    ending_balance = balance
+def monthly_payment(balance, annualInterestRate):
     monthly_payment = 0
-    
+    ending_balance = balance
+
     while ending_balance >= 0:
         monthly_payment += 10
-        ending_balance = debt_payoff_calculation(balance, annualInterestRate, monthly_payment)
+        ending_balance = calculate_end_balance(balance, annualInterestRate, monthly_payment)
     
     return monthly_payment
 
-fixed_montly_payment = find_monthly_payment(balance)
-   
-print(find_monthly_payment(3329, .2)
-
+payment =  monthly_payment(3329, 0.2)
+print(payment)
