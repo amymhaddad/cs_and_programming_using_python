0423be5f92987ed12e57fc4418ec552658a3790b
Amy M Haddad <amy.m.haddad@gmail.com>
2019-03-02 11:57:02 -0500

    added changes to cc_balance problem

--- week2_exercises/cc_balance_bisection_search.py
+++ week2_exercises/cc_balance_bisection_search.py
@@ -2,11 +2,12 @@
 import math
 
 
-def calculate_end_balance(balance, annualInterestRate, monthly_payment):
+def calculate_end_balance(balance, annualInterestRate, payment):
     month = 1
     while month <= 12:
+        # import pdb; pdb.set_trace()
         monthly_interest_rate = (annualInterestRate) / 12.0
-        monthly_unpaid_balance = balance - monthly_payment
+        monthly_unpaid_balance = balance - payment
         new_balance = (monthly_unpaid_balance) + (monthly_interest_rate * monthly_unpaid_balance)
 
         balance = new_balance
@@ -14,71 +15,36 @@ def calculate_end_balance(balance, annualInterestRate, monthly_payment):
 
     return balance
 
+# print(calculate_end_balance(200, .2, 100))
 
 def monthly_payment(balance, annualInterestRate):
 
     monthly_interest_rate = (annualInterestRate) / 12.0
     lowest_payment = balance / 12
     highest_payment = (balance * (1 + monthly_interest_rate) ** 12) / 12.0
-    monthly_payment = (lowest_payment + highest_payment) // 2
+    payment = (lowest_payment + highest_payment) // 2
 
     ending_balance = balance
      
-    while lowest_payment <= highest_payment:
-        ending_balance = calculate_end_balance(balance, annualInterestRate, monthly_payment)
+    while ending_balance >= 0:
 
-        if monthly_payment * 12 == ending_balance:
+        if ending_balance <= 0:
+            # return payment
             break
-
-        else: 
-            if monthly_payment * 12 < ending_balance:
-                lowest_payment = monthly_payment
-            elif monthly_payment * 12 > ending_balance:
-                highest_payment = monthly_payment
-
-        monthly_payment = (lowest_payment + highest_payment) // 2
-
-    return monthly_payment
-
-print(monthly_payment(32000, .2))
-
-
-
-    # while lowest_payment <= highest_payment:
-    #     #getting a neg here
-    #     ending_balance = calculate_end_balance(balance, annualInterestRate, monthly_payment)
+        else:
+            if ending_balance > 0:
+                highest_payment = payment
+            else:
+                lowest_payment = payment
         
-        
-    #     if ending_balance <= 0:
-    #         break
-        
-    #     else:
-    #         if monthly_payment < ending_balance:
-    #             lowest_payment = monthly_payment
-    #         else:
-    #             highest_payment = monthly_payment
-    #     monthly_payment = (lowest_payment + highest_payment) // 2
-
-    #     return monthly_payment
-
-
-
-    # while lowest_payment <= highest_payment:
-    #     calc_ending_balance = calculate_end_balance(balance, annualInterestRate, monthly_payment)
-        
-    #     if mid_payment == monthly_payment:
-    #         break
-        
-    #     else:
-    #         if mid_payment < monthly_payment:
-    #             low = mid_payment
-    #         else:
-    #             high = mid_payment
-        
-    #     return monthly_payment
+        # payment = (lowest_payment + highest_payment) // 2
+        ending_balance = calculate_end_balance(balance, annualInterestRate, payment)
+    
+    return payment
 
 print(monthly_payment(320000, .2))
 
+#Why isn't 2nd function iterating through each ending_balance?
 
 29157.09
 
@@ -97,3 +63,21 @@ print(monthly_payment(320000, .2))
 #    else:
 #       high = mid
 #    mid = (low + high) // 2
+
+
+#Option
+# while lowest_payment <= highest_payment:
+#         ending_balance = calculate_end_balance(balance, annualInterestRate, monthly_payment)
+
+#         if monthly_payment * 12 == ending_balance:
+#             break
+
+#         else: 
+#             if monthly_payment * 12 < ending_balance:
+#                 lowest_payment = monthly_payment
+#             elif monthly_payment * 12 > ending_balance:
+#                 highest_payment = monthly_payment
+
+#         monthly_payment = (lowest_payment + highest_payment) // 2
+
+#     return monthly_payment
