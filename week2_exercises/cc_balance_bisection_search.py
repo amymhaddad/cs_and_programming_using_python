868d9547d7d5dff29daacf0b05e148b2cf212d08
Amy M Haddad <amy.m.haddad@gmail.com>
2019-03-02 14:40:49 -0500

    Small changes to cc_balance WIP

--- week2_exercises/cc_balance_bisection_search.py
+++ week2_exercises/cc_balance_bisection_search.py
@@ -24,15 +24,13 @@ def monthly_payment(balance, annualInterestRate):
 
     ending_balance = balance
      
-    #Use while True, which keeps the loop running. THis is a better option than while ending_balance >= 0 b/c t
-    #It's repetitive with the condition below: ending_balance <= 0
     while True:
         payment = (lowest_payment + highest_payment) // 2
         ending_balance = calculate_end_balance(balance, annualInterestRate, payment)
         print(ending_balance)
 
         if ending_balance <= 0:
-            # return payment
+           
             return payment
         else:
             if ending_balance > 0:
@@ -41,41 +39,3 @@ def monthly_payment(balance, annualInterestRate):
                 highest_payment = payment
 
 monthly_payment(320000, .2)
-
-#Why isn't 2nd function iterating through each ending_balance?
-
-29157.09
-
-# low = 0
-# high = 10
-# mid = (low + high) // 2
-
-# num = 7
-
-# while low <= high:
-#    if mid == num:
-#       print(mid)
-#       break
-#    elif mid < num:
-#       low = mid
-#    else:
-#       high = mid
-#    mid = (low + high) // 2
-
-
-#Option
-# while lowest_payment <= highest_payment:
-#         ending_balance = calculate_end_balance(balance, annualInterestRate, monthly_payment)
-
-#         if monthly_payment * 12 == ending_balance:
-#             break
-
-#         else: 
-#             if monthly_payment * 12 < ending_balance:
-#                 lowest_payment = monthly_payment
-#             elif monthly_payment * 12 > ending_balance:
-#                 highest_payment = monthly_payment
-
-#         monthly_payment = (lowest_payment + highest_payment) // 2
-
-#     return monthly_payment
