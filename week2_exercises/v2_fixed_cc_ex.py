001f4dbea7934602e6079e28dfe89da1182dd7d6
Amy M Haddad <amy.m.haddad@gmail.com>
2019-03-02 14:21:08 -0500

    Final changes to fixed_cc ex

--- week2_exercises/v2_fixed_cc_ex.py
+++ week2_exercises/v2_fixed_cc_ex.py
@@ -1,10 +1,8 @@
 
-
-
 def calculate_end_balance(balance, annualInterestRate, monthly_payment):
     month = 1
     while month <= 12:
-        monthly_interest_rate = (annualInterestRate) / 12.0
+        monthly_interest_rate = annualInterestRate / 12.0
         monthly_unpaid_balance = balance - monthly_payment
         new_balance = (monthly_unpaid_balance) + (monthly_interest_rate * monthly_unpaid_balance)
 
@@ -15,13 +13,11 @@ def calculate_end_balance(balance, annualInterestRate, monthly_payment):
 
 def monthly_payment(balance, annualInterestRate):
     monthly_payment = 0
-    ending_balance = balance
 
-    while ending_balance >= 0:
+    while True:
         monthly_payment += 10
         ending_balance = calculate_end_balance(balance, annualInterestRate, monthly_payment)
-    
-    return monthly_payment
+        if ending_balance <= 0:
+            return monthly_payment
 
-payment =  monthly_payment(3329, 0.2)
-print(payment)
+print(monthly_payment(320000, .2))
