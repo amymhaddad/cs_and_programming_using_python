703c830806e686dfde57ead8f67cebbc1722ea60
Amy M Haddad <amy.m.haddad@gmail.com>
2019-03-08 17:05:45 -0500

    Final version

--- week3_exercises/biggest_ex.py
+++ week3_exercises/biggest_ex.py
@@ -9,12 +9,6 @@ animals['d'] = ['donkey']
 animals['d'].append('dog')
 animals['d'].append('dingo')
 
-aDict = {
-    'a': [], 
-    'b': [1, 7, 5, 4, 3, 18, 10, 0],
-    }
-
-
 def biggest(aDict):
     list_of_value_lengths = [len(val) for val in aDict.values()]
     max_val_length = max(list_of_value_lengths)
@@ -29,4 +23,4 @@ def biggest(aDict):
                 many_values.append(letter)
     return many_values[0]
 
-print(biggest(aDict))
+print(biggest(animals))
